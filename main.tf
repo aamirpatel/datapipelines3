104762e0bbb8fc47446f0e07699a812115460ae4
Aamir Patel <paamir89@gmail.com>
2024-12-25 18:53:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "my-data-bucket71189"
+}
+
+resource "aws_glue_catalog_database" "example" {
+  name = "example_db"
+}
+
+resource "aws_glue_crawler" "example" {
+  name         = "example-crawler"
+  database_name = aws_glue_catalog_database.example.name
+  role_arn     = "arn:aws:iam::123456789012:role/AWSGlueServiceRole"
+  s3_target {
+    path = "s3://${aws_s3_bucket.data_bucket.bucket}/input/"
+  }
+}
+
+resource "aws_glue_job" "example" {
+  name     = "example-job"
+  role_arn = "arn:aws:iam::123456789012:role/AWSGlueServiceRole"
+  command {
+    script_location = "s3://${aws_s3_bucket.data_bucket.bucket}/scripts/example.py"
+    name            = "glueetl"
+  }
+}
